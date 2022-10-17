3cc94ee2fcfbdf2fc9bbd409541c3e21d4e1374f
slamer <411387273@qq.com>
2022-10-17 14:52:51 +0800

    yolox

---
+++ yolox_s.sh
@@ -0,0 +1,1 @@
+python3 tools/demo.py image -n yolox-s  -c exps/pth/yolox_s.pth --path assets/cars.jpg --conf 0.25 --nms 0.45 --tsize 640 --save_result --device gpu
---
+++ yolox_x.sh
@@ -0,0 +1,1 @@
+python3 tools/demo.py image -n yolox-x -c exps/pth/yolox_x.pth --path assets/cars.jpg --conf 0.25 --nms 0.45 --tsize 640 --save_result --device gpu
